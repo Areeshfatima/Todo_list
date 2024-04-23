de3746ba8c868397a31b6211b4e9d27b7c84b1bd
Areeshfatima <muslimahvibes.696@gmail.com>
2024-04-23 08:53:18 +0500

    todo list publish on github

---
+++ index.ts
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos = [];
+let condition1 = true;
+let condition2 = true;
+
+
+while(condition1){
+
+
+let addTodos = await inquirer.prompt(
+    [
+        {
+           name: "firstQuestion" ,
+           type: "input",
+           message: "What would you like to add  in your todos?"
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "Are you sure you want to add something in your todos?",
+            default: "false"
+        },
+    ]
+);
+todos.push(addTodos.firstQuestion);
+condition1 = addTodos.secondQuestion;
+console.log(todos);
+};
+
+while(condition2){
+
+let askUser = await inquirer.prompt(
+    [
+        {
+            name: "thirdQuestion",
+            type: "input",
+            message: "Do you like to remove something in your todos list?"
+
+        },
+        {
+           name: "fourthQuestion",
+           type: "confirm",
+           message: "Are you sure you want to remove something?",
+           default: "false"
+        }
+    ]
+);
+
+todos.pop();
+condition2 = askUser.fourthQuestion;
+console.log(todos);
+};
